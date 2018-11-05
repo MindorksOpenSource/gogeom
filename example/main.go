7fa62242549fd4a1d74d64351ae0e0d89c6ecb9d
hi-manshu <hello2himanshusingh@gmail.com>
2018-11-05 17:07:21 +0530

    Project Setup

---
+++ example/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+
+	geometry "github.com/MindorksOpenSource/gogeom"
+)
+
+func main() {
+	line := geometry.GeneralLine{
+		2, 9, 3,
+	}
+	ellipse := geometry.Ellipse{
+		9, 2,
+	}
+	circle := geometry.RadiusFormOfCircle{
+		2, 3, 4, 5, 6, 7,
+	}
+	fmt.Println(ellipse.GetEccentricity())
+	fmt.Println(line.SlopeOfLine())
+	fmt.Println(line.XIntercept())
+	fmt.Println(line.YIntercept())
+	fmt.Println(circle.AreaOfCircles())
+
+}
